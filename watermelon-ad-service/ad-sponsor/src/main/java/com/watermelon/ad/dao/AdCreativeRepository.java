ccfccd2bdd9a9104d9aceebe11b1eee304ab62c8
zhujiashuai <jiashuai.zhu@ishansong.com>
2019-06-21 11:43:27 +0800

    投放服务Repository

---
+++ watermelon-ad-service/ad-sponsor/src/main/java/com/watermelon/ad/dao/AdCreativeRepository.java
@@ -0,0 +1,6 @@
+package com.watermelon.ad.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdCreativeRepository extends JpaRepository<AdCreativeRepository, Long> {
+}
